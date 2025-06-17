53d7d64e408bbd32b38af65586ea12dc960cca71
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-06-18 01:32:37 +0800

    refactor(web): remove unused constants and social media links from constants file

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,40 +1,8 @@
 import type { Breakpoint } from "@/hooks/use-responsive-image-size";
 import moment from "moment";
 
-export const GITHUB_USERNAME = "1chooo";
-export const MEDIUM_USERNAME = "1chooo";
-export const TWITTER_USERNAME = "1chooo___";
-export const LINKEDIN_USERNAME = "1chooo";
-export const EMAIL = "hugo@1chooo.com";
-
 export const POSTS_PER_PAGE = 8;
 export const BUILD_TIME = moment().format("MMMM Do YYYY");
-
-export const DEVOPS = {
-  linux: "linux",
-  aws: "aws",
-  azure: "azure",
-  githubactions: "githubactions",
-  docker: "docker",
-  fastapi: "fastapi",
-  kubernetes: "kubernetes",
-  flask: "flask",
-  gitlab: "gitlab",
-  redis: "redis",
-};
-
-export const PROGLANG = {
-  python: "py",
-  go: "go",
-  java: "java",
-  cpp: "cpp",
-  c: "c",
-  react: "react",
-  typescript: "typescript",
-  javascript: "javascript",
-  flutter: "flutter",
-  bash: "bash",
-};
 
 export const breakpoints: Breakpoint[] = [
   { maxWidth: 250, size: { width: 80, height: 80 } },
@@ -46,5 +14,3 @@ export const breakpoints: Breakpoint[] = [
 
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
-export const HOME_OG_IMAGE_URL =
-  "https://docs.1chooo.com/images/cover-with-1chooo-com.png";
