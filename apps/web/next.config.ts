33439823d439d68571a5d0e1908ac77a619c047e
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-06-24 00:11:25 +0800

    feat(web): add bundle analyzer configuration and update dependencies (#1204)
    - resolved: #1204

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -16,4 +16,8 @@ const nextConfig: NextConfig = {
   ],
 };
 
-export default nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+ 
+module.exports = withBundleAnalyzer(nextConfig);
