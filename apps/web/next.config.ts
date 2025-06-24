ed1553f6e85025881ee1b06e1998fafa5b123543
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-06-24 15:46:16 +0800

    chore(web): update package versions and clean up next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -16,5 +16,5 @@ const nextConfig: NextConfig = {
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
- 
+
 module.exports = withBundleAnalyzer(nextConfig);
